e288b112303e086a71536f5ae61c83c9b7b2cd07
Gavin Draper <gavdraper@users.noreply.github.com>
2022-12-11 20:44:03 +0000

    Day 11 Tidy Up

--- Day11/DayElevenTest.cs
+++ Day11/DayElevenTest.cs
@@ -1,95 +1,7 @@
 namespace DayEleven;
 
-public class DayElevenTest
+public partial class DayElevenTest
 {
-
-    public class Monkey
-    {
-        public List<Int64> Items { get; set; }
-        private string operation = "";
-        public int TestDivisibleBy { get; set; }
-        private int trueAction;
-        private int falseAction;
-        public Int64 InspectCount { get; set; }
-
-        public Monkey(IEnumerable<Int64> items, string operation, int testDivisibleBy, int trueAction, int falseAction)
-        {
-            Items = items.ToList();
-            this.operation = operation;
-            TestDivisibleBy = testDivisibleBy;
-            this.trueAction = trueAction;
-            this.falseAction = falseAction;
-        }
-
-        public Int64 Throw()
-        {
-            var item = Items[0];
-            Items.Remove(item);
-            return item;
-        }
-
-        public int CalculateDestinationMoney(int no)
-        {
-            if (!Items.Any())
-                return -1;
-
-            InspectCount++;
-            var worryLevel = Items[0];
-            Int64 result = 0;
-            if (operation.Contains('+'))
-            {
-                var sides = operation.Split('+').Select(x =>
-                    Int64.Parse(x
-                        .Trim()
-                        .Replace("old", worryLevel.ToString()))
-                ).ToList();
-                result = sides[0] + sides[1];
-            }
-            if (operation.Contains('*'))
-            {
-                var sides = operation.Split('*').Select(x =>
-                    Int64.Parse(x
-                        .Trim()
-                        .Replace("old", worryLevel.ToString()))
-                ).ToList();
-                result = sides[0] * sides[1];
-            }
-            // var roundedResult = (int)Math.Floor(result / 3f); //Part 1
-            var roundedResult = result;
-            var destination = -1;
-            if (roundedResult % TestDivisibleBy == 0)
-                destination = trueAction;
-            else destination = falseAction;
-            if (result < 0)
-            {
-                Console.WriteLine("Boom");
-                Console.WriteLine(worryLevel);
-                Console.WriteLine(operation);
-                throw (new Exception());
-            }
-            Items[0] = roundedResult;
-            return destination;
-        }
-    }
-
-    public static class MonkeyBuilder
-    {
-        public static Monkey Build(string starting, string operation, string test, string trueAction, string falseAction)
-        {
-            var items = starting.Trim().Replace("Starting items: ", "").Split(',').Select(x => Int64.Parse(x));
-            operation = operation.Replace("Operation: new =", "").Trim();
-            var testInt = int.Parse(test.Trim().Replace("Test: divisible by ", ""));
-            var trueActionMonkey = int.Parse(trueAction.Trim().Replace("If true: throw to monkey ", ""));
-            var falseActionMonkey = int.Parse(falseAction.Trim().Replace("If false: throw to monkey ", ""));
-            return new Monkey(
-                items,
-                operation,
-                testInt,
-                trueActionMonkey,
-                falseActionMonkey
-            );
-        }
-    }
 
     private void playRound(List<Monkey> monkeys)
     {
@@ -101,15 +13,11 @@ public class DayElevenTest
 
         for (var m = 0; m < monkeys.Count(); m++)
         {
-            int ndxItem = 0;
             int totalCount = monkeys[m].Items.Count();
-            while (ndxItem < totalCount)
+            for (var ndxItem = 0; ndxItem < totalCount; ndxItem++)
             {
-                var destination = monkeys[m].CalculateDestinationMoney(m);
-                var item = monkeys[m].Throw();
-                item = item % commonDeviser;
-                monkeys[destination].Items.Add(item);
-                ndxItem++;
+                var destination = monkeys[m].CalculateDestinationMoney();
+                monkeys[destination].Items.Add(monkeys[m].Throw() % commonDeviser);
             }
         }
     }
@@ -122,23 +30,20 @@ public class DayElevenTest
         var i = 0;
         while (i < inputs.Count())
         {
-            monkeys.Add(MonkeyBuilder.Build(
-                    inputs[i + 1],
-                    inputs[i + 2],
-                    inputs[i + 3],
-                    inputs[i + 4],
-                    inputs[i + 5]));
+            monkeys.Add(MonkeyBuilder.Build(inputs[i + 1], inputs[i + 2], inputs[i + 3], inputs[i + 4], inputs[i + 5]));
             i += 7;
         }
 
         for (var round = 0; round < 10000; round++)
             playRound(monkeys);
 
-        var topTwo = monkeys.OrderByDescending(x => x.InspectCount)
-        .Take(2).Select(x => x.InspectCount).ToList();
+        var topTwo = monkeys
+            .Select(x => x.InspectCount)
+            .OrderByDescending(x => x)
+            .Take(2)
+            .Aggregate((a, b) => a * b);
 
-        Int64 levelOfMonkeyBusiness = (topTwo[0] * topTwo[1]);
-        // Assert.Equal(10605, levelOfMonkeyBusiness); //Part1
-        Assert.Equal(2713310158, levelOfMonkeyBusiness);
+        // Assert.Equal(10605, topTwo); //Part1
+        Assert.Equal(15447387620, topTwo);
     }
 }
